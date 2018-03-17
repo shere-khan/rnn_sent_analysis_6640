6accc786fa6807de6d9441ebe8c6cc819f50286c
justin <justin.barry.e@knights.ucf.edu>
2018-03-17 01:13:14 -0400

    created sentences from reviews. saves and loads from pickle. started w2v training

--- preprocess/data.py
+++ preprocess/data.py
@@ -1,7 +1,11 @@
-import os, re
+import os, re, pickle, sys
+import logging
+from nltk.data import load
 from nltk.corpus import stopwords
+from nltk import tokenize
 from bs4 import BeautifulSoup
 from sklearn.feature_extraction.text import CountVectorizer
+from gensim.models import Word2Vec
 
 
 stops = set(stopwords.words('english'))
@@ -14,19 +18,6 @@ def get_files_in_dir(dir):
 
     return files
 
-def processfiles(files):
-    data = list()
-    for file in files:
-        with open(file) as f:
-            for line in f:
-                review = BeautifulSoup(line).get_text()
-                review = remove_emoji_and_nums(review)
-                words = review.lower().split()
-                words = [w for w in words if w not in stops]
-                data.append(" ".join(words))
-
-    return data
-
 def remove_emoji_and_nums(text):
     emojis = """:-) :) :o) :] :3 :c) :> =] 8) =) :} :^) 
     :D 8-D 8D x-D xD X-D XD =-D =D =-3 =3 B^D :( :/ :-( :'( :D :P""".split()
@@ -35,29 +26,94 @@ def remove_emoji_and_nums(text):
 
     return text
 
-if __name__ == '__main__':
-    testneg = '/home/justin/pycharmprojects/rnn_sent_analysis_6640/aclImdb/test/neg'
-    testpos = '/home/justin/pycharmprojects/rnn_sent_analysis_6640/aclImdb/test/pos'
-    trainneg = '/home/justin/pycharmprojects/rnn_sent_analysis_6640/aclImdb/train/neg'
-    trainpos = '/home/justin/pycharmprojects/rnn_sent_analysis_6640/aclImdb/train/pos'
+def processfiles(files, label):
+    data = list()
+    for file in files:
+        with open(file) as f:
+            for line in f:
+                data.append((line, label))
 
-    test_neg_files = get_files_in_dir(testneg)
-    test_neg_proc_reviews = processfiles(test_neg_files)
+    return data
 
+def getwords(review, remove_sw=False):
+    review = BeautifulSoup(review, 'lxml').get_text()
+    review = remove_emoji_and_nums(review)
+    words = review.lower().split()
+    if remove_sw:
+        words = [w for w in words if w not in stops]
 
+    return words
+
+def getsentences(review, tokenizer, remove_sw=False):
+    raws = tokenizer.tokenize(review[0].strip())
+    sents = list()
+    for raw in raws:
+        if len(raw) > 0:
+            sents.append(getwords(raw, remove_sw))
+
+    return sents
+
+def prepreprocessdata():
+    # todo stpl size of trianing set test set
+    # todo note # of pos reviews for train and test is equal
+    cap = -1
+
+    # testneg = '/home/justin/pycharmprojects/rnn_sent_analysis_6640/aclImdb/test/neg'
+    # test_neg_files = get_files_in_dir(testneg)
+    # testdata = processfiles(test_neg_files[:cap], 0)
+
+    # testpos = '/home/justin/pycharmprojects/rnn_sent_analysis_6640/aclImdb/test/pos'
     # test_pos_files = get_files_in_dir(testpos)
-    # test_pos_proc_reviews = processfiles(test_pos_files)
-    #
-    # train_neg_files = get_files_in_dir(trainneg)
-    # test_pos_proc_reviews = processfiles(train_neg_files)
-    #
-    # train_pos_files = get_files_in_dir(trainpos)
-    # test_pos_proc_reviews = processfiles(train_pos_files)
+    # testdata.extend(processfiles(test_pos_files[:cap], 1))
+
+    trainneg = '/home/justin/pycharmprojects/rnn_sent_analysis_6640/aclImdb/train/neg'
+    train_neg_files = get_files_in_dir(trainneg)
+    traindata = processfiles(train_neg_files[:cap], 0)
+
+    trainpos = '/home/justin/pycharmprojects/rnn_sent_analysis_6640/aclImdb/train/pos'
+    train_pos_files = get_files_in_dir(trainpos)
+    traindata.extend(processfiles(train_pos_files[:cap], 1))
 
     # stpl: something to play with
     # todo stpl max_features
-    vectorizer = CountVectorizer(analyzer='word',
-                                 tokenizer=None,
-                                 preprocessor=None,
-                                 stop_words=None,
-                                 max_features=5000)
+    # vectorizer = CountVectorizer(analyzer='word',
+    #                              tokenizer=None,
+    #                              preprocessor=None,
+    #                              stop_words=None)
+
+    tokr = load('tokenizers/punkt/english.pickle')
+
+    sentences = list()
+    for review in traindata:
+        sentences += getsentences(review, tokr)
+
+    return sentences
+
+if __name__ == '__main__':
+    isfilesaved = False
+    f = open(sys.argv[1])
+    isfilesaved = True if f else False
+    if not isfilesaved:
+        sentences = prepreprocessdata()
+        pickle.dump(sentences, open("sentences.p", "wb"))
+    else:
+        sentences = pickle.load(open("sentences.p", "rb"))
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
+                        level=logging.INFO)
+
+    num_features = 300  # Word vector dimensionality
+    min_word_count = 40  # Minimum word count
+    num_workers = 4  # Number of threads to run in parallel
+    context = 10  # Context window size
+    downsampling = 1e-3  # Downsample setting for frequent words
+
+    print("W2V training...")
+    model = Word2Vec(sentences, workers=num_workers, size=num_features,
+                     min_count=min_word_count, window=context, sample=downsampling,
+                     seed=1)
+
+    # If no further training, makes more mem efficient
+    model.init_sims(replace=True)
+
+    # Save the model
+    model.save("word2vecmodel")
